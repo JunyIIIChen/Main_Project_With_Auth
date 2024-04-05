914454847c1d86b94dfc973046da4365e3ccfb59
kai xu <1533543264@qq.com>
2024-04-05 17:51:01 +0800

    first page model

--- Main_Project_With_Auth/ClientApp/src/App.js
+++ Main_Project_With_Auth/ClientApp/src/App.js
@@ -1,16 +1,16 @@
 // App.js
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Calculate_ELE from './Calculate_ELE'; // Calculate_ELE 组件
+import FristPage from './FristPage'; // FristPage 组件
 
 function App() {
   let location = useLocation();
 
   return (
     <div>
-      {/* 仅在非 /calculate_ele 路径时显示这个链接 */}
+      {/* 仅在非 /FristPage 路径时显示这个链接 */}
       {location.pathname == '/' && (
-        <a href="/calculate_ele">Calculate the electricity</a>
+        <a href="/FristPage">FristPage</a>
       )}
       
       {/* 路由配置 */}
@@ -20,7 +20,7 @@ function App() {
             {/* 首页的其他内容 */}
           </div>
         } />
-        <Route path="/calculate_ele" element={<Calculate_ELE />} />
+        <Route path="FristPage" element={<FristPage />} />
         {/* ...其他路由 */}
       </Routes>
     </div>
